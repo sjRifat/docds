dbec3c8b322f5ac12001ab9d53299b6ebb951f03
sjRifat <sj154rifat@gmail.com>
2025-02-09 03:17:16 +0600

    author name added

--- fetch_get_commit.sh
+++ fetch_get_commit.sh
@@ -2,4 +2,6 @@ REPO_URL="https://github.com/sjRifat/portfolio-test.git"
 git clone $REPO_URL
 cd portfolio-test
 git fetch
+author_name=$(git log -1 --format='%aN')
+echo "Author Name: $author_name"
 git log --oneline
